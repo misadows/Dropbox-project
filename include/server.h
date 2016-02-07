a767f737be625192853b9cbd86146f706b5d5965
Michał Sadowski <sadowskimichal95@gmail.com>
2016-02-07 12:13:32 +0100

    implementation of server

---
+++ include/server.h
@@ -0,0 +1,22 @@
+#ifndef server_h
+#define server_h
+#include "communication_utils.h"
+
+#define MAX_CLIENT_NUM 64
+
+typedef struct {
+    int highest_fd;
+    fd_set server_fd_set;
+    int socket;
+    int client_array[MAX_CLIENT_NUM];
+    int client_num;
+} server_t;
+
+void run(server_t server);
+server_t get_server(int socket);
+void register_client(server_t* server);
+void add_client(int socket, int* client_array, int* client_num);
+void handle_file_request(int socket, message_info_t info);
+void handle_client_message(int socket);
+
+#endif
---
+++ src/server.c
@@ -0,0 +1,102 @@
+#include <sys/select.h>
+#include "server.h"
+#include "communication_utils.h"
+#include "socket_utils.h"
+#define SERVER_BOX_FILENAME ".server_box"
+#define LOCAL_BOX_FILENAME ".box"
+
+
+int main(){
+    struct sockaddr addr;
+    server_t server;
+    socklen_t len;
+
+    int socket = init_server_socket(9000);
+    server=get_server(socket);
+    run(server);
+
+    return EXIT_SUCCESS;
+}
+
+
+void run(server_t server){
+    int result, fd;
+    fd_set read_set;
+
+    while(1){
+        read_set = server.server_fd_set;
+        result = select(server.highest_fd+1, &read_set, NULL, NULL, NULL);
+        if(result<0){
+            perror("Select error");
+            exit(EXIT_FAILURE);
+        }
+
+        for(fd=0; fd<=server.highest_fd; fd++){
+            if(FD_ISSET(fd, &read_set))
+            {
+                if(fd==server.socket){
+                    register_client(&server);
+                }
+                else{
+                    handle_client_message(fd);
+                }
+            }
+        }
+    }
+
+}
+
+void manage_clients(int fd, server_t server, fd_set read_set)
+
+server_t get_server(int socket){
+    server_t server;
+    server.socket = socket;
+    FD_ZERO(&server.server_fd_set);
+    FD_SET(socket, &server.server_fd_set);
+    server.highest_fd = socket;
+    server.client_num=0;
+
+    return server;
+}
+
+void register_client(server_t* server){
+    int client_fd;
+    client_fd = accept(server->socket, NULL, 0);
+    if(client_fd<0){
+        perror("accept error");
+        exit(EXIT_FAILURE);
+    }
+    add_client(client_fd, server->client_array, &server->client_num);
+}
+
+void add_client(int socket, int* client_array, int* client_num){
+    if (*client_num >= MAX_CLIENT_NUM){
+        printf("Client add error");
+        exit(0);
+    }
+    client_array[*client_num]=socket;
+    *client_num += 1;
+}
+
+void handle_file_request(int socket, message_info_t info){
+    send_file(socket, info.name, SERVER_FILE);
+}
+
+void handle_client_file(int socket, message_info_t info){
+    box_entry_t* head;
+    head = read_box(SERVER_BOX_FILENAME);
+}
+
+void handle_client_message(int socket){
+    message_info_t info;
+    read(socket, &info, sizeof(info));
+
+    switch(info.message_type){
+        case FILE_REQUEST:
+            handle_file_request(socket, info);
+        case CLIENT_FILE:
+            handle_client_file(socket, info);
+        default:
+            break;
+    }
+}
